419f7883c79228b77713ff91ab2fed80de6ed913
Scalarpy <scalarpy@gmail.com>
2024-10-27 08:05:50 +0530

    Packages update

--- app.py
+++ app.py
@@ -6,7 +6,12 @@ import numpy as np
 from sklearn.preprocessing import OneHotEncoder
 data_dir = "Dataset/"
 
-
+final_columns = ['Var1', 'Online_Follower', 'LinkedIn_Shared', 'Twitter_Shared',
+       'Facebook_Shared', 'Donation', 'Camp_Duration', 'dur_rf', 'RD', 'RM',
+       'RY', 'RW', 'CSD', 'CSM', 'CSY', 'CSW', 'CED', 'CEM', 'CEY', 'CEW',
+       'FID', 'FIM', 'FIY', 'FIW', 'Category1_Second', 'Category1_Third',
+       'Category2_B', 'Category2_C', 'Category2_D', 'Category2_E',
+       'Category2_F', 'Category2_G', 'Category3_2']
 model = joblib.load("health_camp_outcome.pkl")
 sc = model.sc
 oh_map = model.oh_map
@@ -19,8 +24,10 @@ def merge_data(new_data):
     patient_profile = patient_profile.replace("None",np.nan)
     dataset = pd.merge(new_data, camp_det, how = "left", on = "Health_Camp_ID")
     dataset = pd.merge(dataset, patient_profile, how = "left", on = "Patient_ID")
-    fhc.drop(["Health_Score", "Unnamed: 4"], axis = 1,inplace = True)
-    dataset = pd.merge(dataset, fhc, how = "left", on = ["Patient_ID","Health_Camp_ID"])
+    fhc.drop(["Health_Score", "Unnamed: 4","Health_Camp_ID"], axis = 1,inplace = True)
+    dataset = pd.merge(dataset, fhc, how = "left", on = ["Patient_ID"])
+    dataset.drop_duplicates(subset = ["Patient_ID","Health_Camp_ID"], inplace = True)
+    print("dataset merge_data", dataset.shape)
     return dataset
 
 def derive_columns(dataset):
@@ -37,7 +44,7 @@ def derive_columns(dataset):
     dataset["dur_rf"] = dataset["dur_rf"].dt.days
 
     dataset = dataset.drop(["Patient_ID","Health_Camp_ID","City_Type",
-                            "Employer_Category", "Income","Education_Score","Age"],axis=1)
+                        "Employer_Category", "Income","Education_Score","Age"],axis=1)
 
     dataset["RD"] = dataset["Registration_Date"].dt.day
     dataset["RM"] = dataset["Registration_Date"].dt.month
@@ -64,17 +71,22 @@ def derive_columns(dataset):
    
     return dataset
 def one_hot_encode(new_data, categorical_features,oh_map, drop_first = True):
+    each_col_data = []
     for col in categorical_features:
-        oh_encode=pd.DataFrame(oh_map[col].transform(new_data[col].values.reshape(-1,1)).toarray())
+        oh_encode=pd.DataFrame(oh_map[col].transform(new_data[[col]].values).toarray())
         if drop_first:
             oh_encode=oh_encode.iloc[:,1:]
         new_data=new_data.drop(col,axis=1)
-        new_data=pd.concat([new_data,oh_encode],axis=1)
+        each_col_data.append(oh_encode.values)
+    new_data = new_data.values
+    for each_col in each_col_data:
+        new_data=np.concatenate([new_data, each_col], axis = 1)
     return new_data
 
 def pre_process_data(dataset,oh_map, sc):
     
     dataset = one_hot_encode(dataset, ["Category1","Category2","Category3"],oh_map)
+    dataset = pd.DataFrame(dataset, columns =final_columns)
     dataset = sc.transform(dataset)
     return dataset
 
@@ -93,8 +105,10 @@ def main():
         final_data = merge_data(test_data)
         final_data = derive_columns(final_data)
         final_data = pre_process_data(final_data, oh_map, sc)
-        prediction = model.predict(final_data)
+        print("final_data :", final_data.shape)
 
+        prediction = model.predict(final_data)
+        print("prediction", prediction.shape,test_data.shape )
         test_data["outcome"] = prediction
         # Download button
         st.write("### Download Health Camp Outcome Analysis")
